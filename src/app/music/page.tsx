f7a869d1b122b6e058711ff93aee154eab556868
Terence <texzcorp@gmail.com>
2024-10-05 06:52:13 +0200

    Rodage, test d'ajout de la page musique

---
+++ src/app/music/page.tsx
@@ -0,0 +1,70 @@
+import { getPosts } from '@/app/utils';
+import { Flex } from '@/once-ui/components';
+import { Projects } from '@/app/work/components/Projects';
+import { baseURL, person, music } from '../resources';
+
+export function generateMetadata() {
+	const title = music.title;
+	const description = music.description;
+	const ogImage = `https://${baseURL}/og?title=${encodeURIComponent(title)}`;
+
+	return {
+		title,
+		description,
+		openGraph: {
+			title,
+			description,
+			type: 'website',
+			url: `https://${baseURL}/music`,
+			images: [
+				{
+					url: ogImage,
+					alt: title,
+				},
+			],
+		},
+		twitter: {
+			card: 'summary_large_image',
+			title,
+			description,
+			images: [ogImage],
+		},
+	};
+}
+
+export default function music() {
+    let allProjects = getPosts(['src', 'app', 'music', 'projects']);
+
+    return (
+        <Flex
+			fillWidth maxWidth="m"
+			direction="column">
+            <script
+                type="application/ld+json"
+                suppressHydrationWarning
+                dangerouslySetInnerHTML={{
+                    __html: JSON.stringify({
+                        '@context': 'https://schema.org',
+                        '@type': 'CollectionPage',
+                        headline: music.title,
+                        description: music.description,
+                        url: `https://${baseURL}/projects`,
+                        image: `${baseURL}/og?title=Design%20Projects`,
+                        author: {
+                            '@type': 'Person',
+                            name: person.name,
+                        },
+                        hasPart: allProjects.map(project => ({
+                            '@type': 'Creativemusic',
+                            headline: project.metadata.title,
+                            description: project.metadata.summary,
+                            url: `https://${baseURL}/projects/${project.slug}`,
+                            image: `${baseURL}/${project.metadata.image}`,
+                        })),
+                    }),
+                }}
+            />
+            <Projects/>
+        </Flex>
+    );
+}
