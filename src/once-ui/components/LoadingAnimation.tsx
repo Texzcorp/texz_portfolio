768ecfed2a00bae99cfe18776e7b8c47fb2012ee
Terence <texzcorp@gmail.com>
2024-10-27 23:27:30 +0100

    Animation improvement

--- src/once-ui/components/LoadingAnimation.tsx
+++ src/once-ui/components/LoadingAnimation.tsx
@@ -7,10 +7,9 @@ const LoadingAnimation = () => {
     return (
         <div className={styles.loadingContainer}>
             <div className={styles.glassBox}>
-                <div className={styles.loadingDot}></div>
-                <div className={styles.loadingDot}></div>
-                <div className={styles.loadingDot}></div>
-                <div className={styles.glowEffect}></div>
+                <div className={styles.orb}></div>
+                <div className={styles.orbTrail}></div>
+                <div className={styles.glowRing}></div>
             </div>
         </div>
     );
