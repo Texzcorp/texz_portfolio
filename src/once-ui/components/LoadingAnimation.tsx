015b374d44d5239f1e1e40458bef2bfbbc4c0ec9
Terence <texzcorp@gmail.com>
2024-10-27 23:22:21 +0100

    loading design improvement

--- src/once-ui/components/LoadingAnimation.tsx
+++ src/once-ui/components/LoadingAnimation.tsx
@@ -6,9 +6,12 @@ import styles from './LoadingAnimation.module.scss';
 const LoadingAnimation = () => {
     return (
         <div className={styles.loadingContainer}>
-            <div className={styles.loadingDot}></div>
-            <div className={styles.loadingDot}></div>
-            <div className={styles.loadingDot}></div>
+            <div className={styles.glassBox}>
+                <div className={styles.loadingDot}></div>
+                <div className={styles.loadingDot}></div>
+                <div className={styles.loadingDot}></div>
+                <div className={styles.glowEffect}></div>
+            </div>
         </div>
     );
 };
