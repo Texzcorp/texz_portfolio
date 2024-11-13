cc06e6fd175c972ab53a469265e9c16fd6b44f43
Terence <texzcorp@gmail.com>
2024-11-13 18:53:16 +0100

    Update ProjectCard.tsx

--- src/app/components/ProjectCard.tsx
+++ src/app/components/ProjectCard.tsx
@@ -108,7 +108,7 @@ export const ProjectCard: React.FC<ProjectCardProps> = ({
                         alt={title}
                         aspectRatio="16 / 9"
                         src={images[activeIndex]}
-                        variant="glow"
+                        variant="default"
                         priority={activeIndex === 0} // Priorité pour la première image
                         style={{
                             border: '1px solid var(--neutral-alpha-weak)',
